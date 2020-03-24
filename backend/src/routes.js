90943754144a46dd7a32489bdd1743c4db87afa9
simpletextbr <simpletextbr@gmail.com>
2020-03-24 20:31:23 -0300

    Backend da aplicacao

---
+++ backend/src/routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const OngController = require('./controller/OngController');
+const IncidentController = require('./controller/IncidentController');
+const ProfileController = require('./controller/ProfileController');
+const SessionController = require('./controller/SessionController');
+
+
+const routes = express.Router();
+
+routes.post('/session', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profiles', ProfileController.index);
+
+
+
+
+
+
+module.exports = routes;
